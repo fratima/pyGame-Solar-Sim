70418337499f804ebe80cdeaa438de18fab42d64
fratima <fratima.ist@abwesend.de>
2023-12-19 21:02:17 +0100

    initial

--- myPyGame.py
+++ myPyGame.py
@@ -8,7 +8,7 @@ ORANGE  = ( 255, 140, 0)
 ROT     = ( 255, 0, 0)
 GRUEN   = ( 0, 255, 0)
 SCHWARZ = ( 0, 0, 0)
-WEISS   = ( 255, 255, 255)
+WEISS   = ( 255, 255, 255) 
 
 #------ Pygame stuff --------------
 pygame.init()
